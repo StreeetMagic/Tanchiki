e530159cfad87cbef6d6297d7ace45785772f37e
Vlad Parastaev <streeetmagic@gmail.com>
2024-05-01 22:36:04 +0400

    add W A movement

--- Assets/Scripts/Control/TankController.cs
+++ Assets/Scripts/Control/TankController.cs
@@ -7,25 +7,44 @@ public class TankController : MonoBehaviour
 
     private int state = 0;
     private float horizontalInput = 0;
+    private float verticalInput = 0;
+    
     private void Start()
     {
         EnemyGeneral.Instance.SetPlayerTank(this);
     }
     private void Update()
     {
-        if (Input.GetKey("up"))
-        {
-            state = 1;
-        }
-        else if (Input.GetKey("down"))
-        {
-            state = -1;
-        }
-        else
-        {
-            state = 0;
-        }
+         verticalInput = Input.GetAxis("Vertical");
+         
+         if (verticalInput > 0)
+         {
+             state = 1;
+         }
+         else if (verticalInput < 0)
+         {
+             state = -1;
+         }
+         else
+         {
+             state = 0;
+         }
+        
+        // if (Input.GetKey("up"))
+        // {
+        //     state = 1;
+        // }
+        // else if (Input.GetKey("down"))
+        // {
+        //     state = -1;
+        // }
+        // else
+        // {
+        //     state = 0;
+        // }
+        
         horizontalInput = Input.GetAxis("Horizontal");
+        
         if (Input.GetButtonDown("Fire1"))
         {
             fire.Launch();
